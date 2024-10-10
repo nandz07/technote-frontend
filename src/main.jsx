4ad1a9dbf9aa6371de056920adfb375051c59cb3
Nandakumar T <nandakumart07@gmail.com>
2024-10-10 11:27:54 +0530

    feat:redux , store , slices

--- src/main.jsx
+++ src/main.jsx
@@ -4,20 +4,17 @@ import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layouts from './layouts/Layouts.jsx'
 import App from './App.jsx'
+import { store } from './app/store.js'
+import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-    {/* <ScrollToTop /> */}
-      {/* <AuthContextProvider> */}
-        {/* <ToastContainer theme='dark' className='mt-20' position='top-right' autoClose={3000} closeOnClick pauseOnHover={false} /> */}
+    <Provider store={store}>
+      <BrowserRouter>
         <Routes>
-          {/* <Route path='*' element={<Layouts />} /> */}
-          <Route path='*' element={<App />} />
-          {/* <Route path='/admin/*' element={<AdminLayouts />} />
-          <Route path='/admin' element={<AdminLogin />} /> */}
+          <Route path='/*' element={<App />} />
         </Routes>
-      {/* </AuthContextProvider> */}
-    </BrowserRouter>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
