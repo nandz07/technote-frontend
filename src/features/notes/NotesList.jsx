08d19c3c66c1ce1d60125c025ff00277ee49b792
Nandakumar T <nandakumart07@gmail.com>
2024-11-10 23:35:01 +0530

    feat:page title manage hook

--- src/features/notes/NotesList.jsx
+++ src/features/notes/NotesList.jsx
@@ -1,9 +1,11 @@
 import { useGetNotesQuery } from "./notesApiSlice"
 import Note from "./Note"
 import useAuth from "../../hooks/useAuth"
+import useTitle from "../../hooks/useTitle"
 
 const NotesList = () => {
 
+    useTitle('techNotes: Notes List')
     const { username, isManager, isAdmin } = useAuth()
 
     const {
