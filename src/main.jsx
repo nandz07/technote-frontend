f6e9781d475fd90a8ab31ef677f5f78242a68027
Nandakumar T <nandakumart07@gmail.com>
2024-11-19 07:50:42 +0530

    Ready to deploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,9 @@ import App from './App.jsx'
 import { store } from './app/store.js'
 import { Provider } from 'react-redux'
 // import { PersistGate } from 'redux-persist/integration/react'
+import {disableReactDevTools} from '@fvilers/disable-react-devtools'
+
+if(process.env.NODE_ENV === 'production') disableReactDevTools()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
