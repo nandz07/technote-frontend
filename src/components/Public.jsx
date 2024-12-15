fe6477cd44c76a4e3dc444c15b46d4b69ad86686
Nandakumar T <nandakumart07@gmail.com>
2024-12-15 15:33:49 +0530

    main page update

--- src/components/Public.jsx
+++ src/components/Public.jsx
@@ -5,18 +5,18 @@ const Public = () => {
     const content = (
         <section className="public">
             <header>
-                <h1>Welcome to <span className="nowrap">Dan D. Repairs!</span></h1>
+                <h1>Welcome to <span className="nowrap">Appu's Project Tracker!</span></h1>
             </header>
             <main className="public__main">
-                <p>Located in Beautiful Downtown Foo City, Dan D. Repairs  provides a trained staff ready to meet your tech repair needs.</p>
+                <p>Stay Organized, Collaborate Better, Achieve More</p>
                 <address className="public__addr">
-                    Dan D. Repairs<br />
-                    555 Foo Drive<br />
-                    Foo City, CA 12345<br />
+                Create, assign, and track tasks with ease.<br />
+                    Keep everyone aligned and working toward shared goals.<br />
+                    Organize your team by assigning roles and responsibilities.<br />
                     <a href="tel:+15555555555">(555) 555-5555</a>
                 </address>
                 <br />
-                <p>Owner: Dan Davidson</p>
+                <p>Owner: Nandakumar T</p>
             </main>
             <footer>
                 <Link to="/login">Employee Login</Link>
